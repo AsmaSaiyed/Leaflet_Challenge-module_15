4c78492bca4f83702a22cc71c00930bbb2818f88
AsmaSaiyed <134350326+AsmaSaiyed@users.noreply.github.com>
2023-10-02 16:18:22 -0400

    commit 2

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,31 +1,31 @@
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-// var Mymap = L.map("map", {
-// center: [37.0902, -110.7129],
-// zoom: 5});
-
+ 
 
 let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
 
-d3.json(url).then(function (data) {
-  // Once we get a response, send the data.features object to the createFeatures function.
+d3.json(url). then (function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
+  // console.log(data);
+});
 
-// console.log(data);
+
 
 function createFeatures(earthquakeData) {
 var earthquakes = L.geoJSON(earthquakeData, {
   onEachFeature: function(feature, layer) {
     layer.bindPopup("<h3>Magnitude: " + feature.properties.mag +"</h3><h3>Location: "+ feature.properties.place +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  },
+  }})
+
   function getRadius(value){
     return value*25000
   }
-
+  
   function getColor(d){
     return d > 5 ? "#a54500":
     d  > 4 ? "#cc5500":
@@ -35,7 +35,7 @@ var earthquakes = L.geoJSON(earthquakeData, {
              "#ffcca5";
   }
 
-  pointToLayer: function (feature, latlng) {
+ function  pointtolayer(feature, latlng) {
     return new L.circle(latlng,
       {radius: getRadius(feature.properties.mag),
       fillColor: getColor(feature.properties.mag),
@@ -43,47 +43,42 @@ var earthquakes = L.geoJSON(earthquakeData, {
       color: "#000",
       stroke: true,
       weight: .8
-  })}
-})
-}});
+    })
+  };
+
   createMap(earthquakes);
+}
+
 
 
 function createMap(earthquakes) {
-
-  // Create the base layers.
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
-
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
+//   let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+//     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+//   });
 
   // Create a baseMaps object.
   let baseMaps = {
     "Street Map": street,
-    "Topographic Map": topo
-  };
+   };
 
-  // Create an overlay object to hold our overlay.
+//   // Create an overlay object to hold our overlay.
   let overlayMaps = {
-    Earthquakes: earthquakes
-  };
+   Earthquakes: earthquakes
+ };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
   var Mymap = L.map("map", {
     center: [37.0902, -110.7129],
     zoom: 5,
-    layers: [street, earthquakes]
+    layers: [street,earthquakes]
   });
 
   // Create a layer control.
   // Pass it our baseMaps and overlayMaps.
   // Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(Mymap);
+  // L.control.layers(baseMaps, overlayMaps, {
+  //   collapsed: false
+  // }).addTo(Mymap);
 
    //Create a legend on the bottom left
    var legend = L.control({position: 'bottomleft'});
@@ -102,5 +97,8 @@ function createMap(earthquakes) {
    return div;
  };
 
- legend.addTo(myMap);
-};
+ legend.addTo(Mymap);
+}
+
+
+
